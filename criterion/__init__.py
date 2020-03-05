1f1f4aa75e39964a3182186737026e54f56b701b
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-03-05 07:48:20 +0000

    Incremented minor version,

---
+++ criterion/__init__.py
@@ -0,0 +1,2 @@
+from torch.nn import L1Loss, MSELoss, CrossEntropyLoss, CTCLoss, NLLLoss, PoissonNLLLoss, GaussianNLLLoss, \
+    KLDivLoss, BCELoss, BCEWithLogitsLoss, MarginRankingLoss, HingeEmbeddingLoss, Mul
