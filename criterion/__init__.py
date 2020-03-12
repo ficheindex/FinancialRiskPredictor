25975ed7e1d8436f57a53cf7e29346a385271493
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-03-12 15:34:07 +0000

    Fix #65

--- criterion/__init__.py
+++ criterion/__init__.py
@@ -1,2 +1,6 @@
 from torch.nn import L1Loss, MSELoss, CrossEntropyLoss, CTCLoss, NLLLoss, PoissonNLLLoss, GaussianNLLLoss, \
-    KLDivLoss, BCELoss, BCEWithLogitsLoss, MarginRankingLoss, HingeEmbeddingLoss, Mul
+    KLDivLoss, BCELoss, BCEWithLogitsLoss, MarginRankingLoss, HingeEmbeddingLoss, MultiLabelMarginLoss, \
+    HuberLoss, SmoothL1Loss, SoftMarginLoss, MultiLabelSoftMarginLoss, CosineEmbeddingLoss, MultiMarginLoss, \
+    TripletMarginLoss, TripletMarginWithDistanceLoss
+
+CRITERION_DICT = 
