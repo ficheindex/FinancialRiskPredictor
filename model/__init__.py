6792bcf11513c5d3d38bb84de78e3f37450f7299
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-06-07 17:49:24 +0000

    Remove debug line.

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,13 @@
 from .base import ModelSML, ModelNN, ModelGNN
 from .sml import ModelLogisticRegression, ModelPerceptron, ModelRidgeClassifier, ModelLasso, ModelSGDClassifier, \
     ModelBernoulliNB, ModelMultinomialNB, ModelGaussianNB, ModelDecisionTreeClassifier, \
-    ModelGradientBoostingClassifier, ModelRandomFor
+    ModelGradientBoostingClassifier, ModelRandomForestClassifier, ModelXGBClassifier, \
+    ModelCatBoostClassifier, ModelLGBMClassifier
+from .deepfm import DeepFM
+from .stg import STG
+from .vime import VIME
+from .tabnet import TabNet
+
+
+MODEL_DICT = {
+    "ModelSML": ModelSML, "SML": M
