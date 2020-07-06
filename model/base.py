fb3e3b8cb021eb8b40a5c9c4f7e1850954734e26
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-07-06 12:24:50 +0000

    chore(release): 0.5.1

--- model/base.py
+++ model/base.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+__author__ = "@YuweiYin"
+"""
+
+# import torch
+import torch.nn as nn
+# import torch.nn.functional as F
+from sklearn.base import BaseEstimator
+
+
+class ModelSML(BaseEstimator):
+
+    def __init__(self, args):
+        self.a
