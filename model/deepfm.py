353d2a28ce7b9dbefcfb700373752de9a802df75
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-07-10 14:14:54 +0000

    prepare for release.

--- model/deepfm.py
+++ model/deepfm.py
@@ -4,4 +4,9 @@ from abc import ABC
 import numpy as np
 import torch
 
-f
+from model.basemodel_torch import BaseModelTorch
+from model.deepfm_lib.models.deepfm import DeepFM as DeepFMModel
+from model.deepfm_lib.inputs import SparseFeat, DenseFeat
+
+"""
+DeepFM: A Factorization-Machine based Neu
