694005a33c36e69263556dc0dd10015b39777f3e
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-08-08 21:08:47 +0000

    sparc32: add ucmpdi2

--- model/deepfm_lib/layers/activation.py
+++ model/deepfm_lib/layers/activation.py
@@ -8,4 +8,11 @@ class Dice(nn.Module):
     """The Data Adaptive Activation Function in DIN,which can be viewed as a generalization of PReLu and can adaptively adjust the rectified point according to distribution of input data.
 
     Input shape:
-       
+        - 2 dims: [batch_size, embedding_size(features)]
+        - 3 dims: [batch_size, num_features, embedding_size(features)]
+
+    Output shape:
+        - Same shape as input.
+
+    References
+        - [Zhou G, Zhu X, Song C, et al. 
