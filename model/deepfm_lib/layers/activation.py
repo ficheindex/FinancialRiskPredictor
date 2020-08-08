69de396690923c71860f3becab473e62f5c613a5
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-08-08 18:55:39 +0000

    Update main_application.css

--- model/deepfm_lib/layers/activation.py
+++ model/deepfm_lib/layers/activation.py
@@ -4,4 +4,8 @@ import torch
 import torch.nn as nn
 
 
-class Dice(nn.Mo
+class Dice(nn.Module):
+    """The Data Adaptive Activation Function in DIN,which can be viewed as a generalization of PReLu and can adaptively adjust the rectified point according to distribution of input data.
+
+    Input shape:
+       
