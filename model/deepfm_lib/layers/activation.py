a032611f91451fcdebbf9421080f4f4ba69b3998
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-08-09 21:51:10 +0000

    Compatibility wih latest update

--- model/deepfm_lib/layers/activation.py
+++ model/deepfm_lib/layers/activation.py
@@ -16,4 +16,9 @@ class Dice(nn.Module):
 
     References
         - [Zhou G, Zhu X, Song C, et al. Deep interest network for click-through rate prediction[C]//Proceedings of the 24th ACM SIGKDD International Conference on Knowledge Discovery & Data Mining. ACM, 2018: 1059-1068.](https://arxiv.org/pdf/1706.06978.pdf)
-    
+        - https://github.com/zhougr1993/DeepInterestNetwork, https://github.com/fanoping/DIN-pytorch
+    """
+
+    def __init__(self, emb_size, dim=2, epsilon=1e-8, device='cpu'):
+        super(Dice, self).__init__()
+        assert 
