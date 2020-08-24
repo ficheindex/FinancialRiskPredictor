a116a29e4229d3ecbc7b42cf6d8b134ece828639
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-08-24 22:50:07 +0000

    usb: gadget: u_ether: fix kworker 100% CPU issue with still used interfaces in eth_stop

--- model/deepfm_lib/layers/core.py
+++ model/deepfm_lib/layers/core.py
@@ -4,4 +4,11 @@ import math
 
 import torch
 import torch.nn as nn
-import torch.nn.f
+import torch.nn.functional as F
+
+from model.deepfm_lib.layers.activation import activation_layer
+
+
+class LocalActivationUnit(nn.Module):
+    """The LocalActivationUnit used in DIN with which the representation of
+        user interests varies a
