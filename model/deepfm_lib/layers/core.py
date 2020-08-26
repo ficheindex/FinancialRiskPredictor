e3971f03b5e1f67a0b1759d8b5957f69ba32c9bb
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-08-26 07:28:55 +0000

    Update NOTES

--- model/deepfm_lib/layers/core.py
+++ model/deepfm_lib/layers/core.py
@@ -11,4 +11,10 @@ from model.deepfm_lib.layers.activation import activation_layer
 
 class LocalActivationUnit(nn.Module):
     """The LocalActivationUnit used in DIN with which the representation of
-        user interests varies a
+        user interests varies adaptively given different candidate items.
+
+    Input shape
+        - A list of two 3D tensor with shape:  ``(batch_size, 1, embedding_size)`` and ``(batch_size, T, embedding_size)``
+
+    Output shape
+        - 3D te
