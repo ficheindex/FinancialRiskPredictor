fc2624afb227977b1f5f8176e2083c469cd1dbec
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-10-24 06:57:52 +0000

    vfs: d_obtain_alias() needs to use "/" as default name.

--- model/deepfm_lib/layers/core.py
+++ model/deepfm_lib/layers/core.py
@@ -171,3 +171,10 @@ class Conv2dSame(nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1,
                  padding=0, dilation=1, groups=1, bias=True):
         super(Conv2dSame, self).__init__(
+            in_channels, out_channels, kernel_size, stride, 0, dilation,
+            groups, bias)
+        nn.init.xavier_uniform_(self.weight)
+
+    def forward(self, x):
+        ih, iw = x.size()[-2:]
+    
