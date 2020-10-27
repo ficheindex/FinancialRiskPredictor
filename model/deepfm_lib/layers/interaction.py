5f8c56d9464e6cba035a287cb398a1fea4361bca
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-10-27 17:59:35 +0000

    Fixed indentation.

--- model/deepfm_lib/layers/interaction.py
+++ model/deepfm_lib/layers/interaction.py
@@ -59,3 +59,203 @@ class BiInteractionPooling(nn.Module):
         square_of_sum = torch.pow(
             torch.sum(concated_embeds_value, dim=1, keepdim=True), 2)
         sum_of_square = torch.sum(
+            concated_embeds_value * concated_embeds_value, dim=1, keepdim=True)
+        cross_term = 0.5 * (square_of_sum - sum_of_square)
+        return cross_term
+
+
+class SENETLayer(nn.Module):
+    """SENETLayer used in FiBiNET.
+      Input shape
+        - A list of 3D tensor with shape: ``(batch_size,filed_size,embedding_size)``.
+      Output shape
+        - A list of 3D tensor with shape: ``(batch_size,filed_size,embedding_size)``.
+      Arguments
+        - **filed_size** : Positive integer, number of feature groups.
+        - **reduction_ratio** : Positive integer, dimensionality of the
+         attention network output space.
+        - **seed** : A Python integer to use as random seed.
+      References
+        - [FiBiNET: Combining Feature Importance and Bilinear feature Interaction for Click-Through Rate Prediction
+Tongwen](https://arxiv.org/pdf/1905.09433.pdf)
+    """
+
+    def __init__(self, filed_size, reduction_ratio=3, seed=1024, device='cpu'):
+        super(SENETLayer, self).__init__()
+        self.seed = seed
+        self.filed_size = filed_size
+        self.reduction_size = max(1, filed_size // reduction_ratio)
+        self.excitation = nn.Sequential(
+            nn.Linear(self.filed_size, self.reduction_size, bias=False),
+            nn.ReLU(),
+            nn.Linear(self.reduction_size, self.filed_size, bias=False),
+            nn.ReLU()
+        )
+        self.to(device)
+
+    def forward(self, inputs):
+        if len(inputs.shape) != 3:
+            raise ValueError(
+                "Unexpected inputs dimensions %d, expect to be 3 dimensions" % (len(inputs.shape)))
+        Z = torch.mean(inputs, dim=-1, out=None)
+        A = self.excitation(Z)
+        V = torch.mul(inputs, torch.unsqueeze(A, dim=2))
+
+        return V
+
+
+class BilinearInteraction(nn.Module):
+    """BilinearInteraction Layer used in FiBiNET.
+      Input shape
+        - A list of 3D tensor with shape: ``(batch_size,filed_size, embedding_size)``.
+      Output shape
+        - 3D tensor with shape: ``(batch_size,filed_size*(filed_size-1)/2, embedding_size)``.
+      Arguments
+        - **filed_size** : Positive integer, number of feature groups.
+        - **embedding_size** : Positive integer, embedding size of sparse features.
+        - **bilinear_type** : String, types of bilinear functions used in this layer.
+        - **seed** : A Python integer to use as random seed.
+      References
+        - [FiBiNET: Combining Feature Importance and Bilinear feature Interaction for Click-Through Rate Prediction
+Tongwen](https://arxiv.org/pdf/1905.09433.pdf)
+    """
+
+    def __init__(self, filed_size, embedding_size, bilinear_type="interaction", seed=1024, device='cpu'):
+        super(BilinearInteraction, self).__init__()
+        self.bilinear_type = bilinear_type
+        self.seed = seed
+        self.bilinear = nn.ModuleList()
+        if self.bilinear_type == "all":
+            self.bilinear = nn.Linear(
+                embedding_size, embedding_size, bias=False)
+        elif self.bilinear_type == "each":
+            for _ in range(filed_size):
+                self.bilinear.append(
+                    nn.Linear(embedding_size, embedding_size, bias=False))
+        elif self.bilinear_type == "interaction":
+            for _, _ in itertools.combinations(range(filed_size), 2):
+                self.bilinear.append(
+                    nn.Linear(embedding_size, embedding_size, bias=False))
+        else:
+            raise NotImplementedError
+        self.to(device)
+
+    def forward(self, inputs):
+        if len(inputs.shape) != 3:
+            raise ValueError(
+                "Unexpected inputs dimensions %d, expect to be 3 dimensions" % (len(inputs.shape)))
+        inputs = torch.split(inputs, 1, dim=1)
+        if self.bilinear_type == "all":
+            p = [torch.mul(self.bilinear(v_i), v_j)
+                 for v_i, v_j in itertools.combinations(inputs, 2)]
+        elif self.bilinear_type == "each":
+            p = [torch.mul(self.bilinear[i](inputs[i]), inputs[j])
+                 for i, j in itertools.combinations(range(len(inputs)), 2)]
+        elif self.bilinear_type == "interaction":
+            p = [torch.mul(bilinear(v[0]), v[1])
+                 for v, bilinear in zip(itertools.combinations(inputs, 2), self.bilinear)]
+        else:
+            raise NotImplementedError
+        return torch.cat(p, dim=1)
+
+
+class CIN(nn.Module):
+    """Compressed Interaction Network used in xDeepFM.
+      Input shape
+        - 3D tensor with shape: ``(batch_size,field_size,embedding_size)``.
+      Output shape
+        - 2D tensor with shape: ``(batch_size, featuremap_num)`` ``featuremap_num =  sum(self.layer_size[:-1]) // 2 + self.layer_size[-1]`` if ``split_half=True``,else  ``sum(layer_size)`` .
+      Arguments
+        - **filed_size** : Positive integer, number of feature groups.
+        - **layer_size** : list of int.Feature maps in each layer.
+        - **activation** : activation function name used on feature maps.
+        - **split_half** : bool.if set to False, half of the feature maps in each hidden will connect to output unit.
+        - **seed** : A Python integer to use as random seed.
+      References
+        - [Lian J, Zhou X, Zhang F, et al. xDeepFM: Combining Explicit and Implicit Feature Interactions for Recommender Systems[J]. arXiv preprint arXiv:1803.05170, 2018.] (https://arxiv.org/pdf/1803.05170.pdf)
+    """
+
+    def __init__(self, field_size, layer_size=(128, 128), activation='relu', split_half=True, l2_reg=1e-5, seed=1024,
+                 device='cpu'):
+        super(CIN, self).__init__()
+        if len(layer_size) == 0:
+            raise ValueError(
+                "layer_size must be a list(tuple) of length greater than 1")
+
+        self.layer_size = layer_size
+        self.field_nums = [field_size]
+        self.split_half = split_half
+        self.activation = activation_layer(activation)
+        self.l2_reg = l2_reg
+        self.seed = seed
+
+        self.conv1ds = nn.ModuleList()
+        for i, size in enumerate(self.layer_size):
+            self.conv1ds.append(
+                nn.Conv1d(self.field_nums[-1] * self.field_nums[0], size, 1))
+
+            if self.split_half:
+                if i != len(self.layer_size) - 1 and size % 2 > 0:
+                    raise ValueError(
+                        "layer_size must be even number except for the last layer when split_half=True")
+
+                self.field_nums.append(size // 2)
+            else:
+                self.field_nums.append(size)
+
+        #         for tensor in self.conv1ds:
+        #             nn.init.normal_(tensor.weight, mean=0, std=init_std)
+        self.to(device)
+
+    def forward(self, inputs):
+        if len(inputs.shape) != 3:
+            raise ValueError(
+                "Unexpected inputs dimensions %d, expect to be 3 dimensions" % (len(inputs.shape)))
+        batch_size = inputs.shape[0]
+        dim = inputs.shape[-1]
+        hidden_nn_layers = [inputs]
+        final_result = []
+
+        for i, size in enumerate(self.layer_size):
+            # x^(k-1) * x^0
+            x = torch.einsum(
+                'bhd,bmd->bhmd', hidden_nn_layers[-1], hidden_nn_layers[0])
+            # x.shape = (batch_size , hi * m, dim)
+            x = x.reshape(
+                batch_size, hidden_nn_layers[-1].shape[1] * hidden_nn_layers[0].shape[1], dim)
+            # x.shape = (batch_size , hi, dim)
+            x = self.conv1ds[i](x)
+
+            if self.activation is None or self.activation == 'linear':
+                curr_out = x
+            else:
+                curr_out = self.activation(x)
+
+            if self.split_half:
+                if i != len(self.layer_size) - 1:
+                    next_hidden, direct_connect = torch.split(
+                        curr_out, 2 * [size // 2], 1)
+                else:
+                    direct_connect = curr_out
+                    next_hidden = 0
+            else:
+                direct_connect = curr_out
+                next_hidden = curr_out
+
+            final_result.append(direct_connect)
+            hidden_nn_layers.append(next_hidden)
+
+        result = torch.cat(final_result, dim=1)
+        result = torch.sum(result, -1)
+
+        return result
+
+
+class AFMLayer(nn.Module):
+    """Attentonal Factorization Machine models pairwise (order-2) feature
+    interactions without linear term and bias.
+      Input shape
+        - A list of 3D tensor with shape: ``(batch_size,1,embedding_size)``.
+      Output shape
+        - 2D tensor with shape: ``(batch_size, 1)``.
+      Arguments
