bf3dd33a9734ee1e4d2ac862acbea5647edc6111
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-10-27 08:10:04 +0000

    comparison

--- model/deepfm_lib/layers/core.py
+++ model/deepfm_lib/layers/core.py
@@ -181,4 +181,7 @@ class Conv2dSame(nn.Conv2d):
         oh = math.ceil(ih / self.stride[0])
         ow = math.ceil(iw / self.stride[1])
         pad_h = max((oh - 1) * self.stride[0] + (kh - 1) * self.dilation[0] + 1 - ih, 0)
-       
+        pad_w = max((ow - 1) * self.stride[1] + (kw - 1) * self.dilation[1] + 1 - iw, 0)
+        if pad_h > 0 or pad_w > 0:
+            x = F.pad(x, [pad_w // 2, pad_w - pad_w // 2, pad_h // 2, pad_h - pad_h // 2])
+    
