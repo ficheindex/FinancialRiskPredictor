ba98228c6b959bee24d604ea4dc729d74958e0e5
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-11-11 06:19:59 +0000

    eCryptfs: Initialize empty lower files when opening them

--- model/deepfm_lib/layers/utils.py
+++ model/deepfm_lib/layers/utils.py
@@ -1,4 +1,15 @@
 # -*- coding:utf-8 -*-
 
 import numpy as np
-import to
+import torch
+
+
+def concat_fun(inputs, axis=-1):
+    if len(inputs) == 1:
+        return inputs[0]
+    else:
+        return torch.cat(inputs, dim=axis)
+
+
+def slice_arrays(arrays, start=None, stop=None):
+    """Slice an array or list of 
