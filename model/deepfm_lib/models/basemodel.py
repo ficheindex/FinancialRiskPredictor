807a742c4e09e444511cd763c2b442e451f8b0b0
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-11-23 17:47:38 +0000

    interface cleanup

--- model/deepfm_lib/models/basemodel.py
+++ model/deepfm_lib/models/basemodel.py
@@ -11,4 +11,8 @@ import torch.nn.functional as F
 import torch.utils.data as Data
 from sklearn.metrics import *
 from torch.utils.data import DataLoader
-from tqd
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+
+from model.deepfm_lib.inputs import build_input_features, SparseFeat, DenseFeat, \
+    VarLenSparseFeat, get_varlen_pooling_list, create_embedding_matrix,
