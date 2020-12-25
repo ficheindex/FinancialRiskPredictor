32b7d7fa5bce0013c81a053d80b5bb7a00c7dd1a
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-12-25 09:52:30 +0000

    xtensa: fix kernel register spilling

--- model/deepfm_lib/models/basemodel.py
+++ model/deepfm_lib/models/basemodel.py
@@ -128,4 +128,4 @@ class BaseModel(nn.Module):
             dictionary mapping input names to Numpy arrays.
         :param y: Numpy array of target (label) data (if the model has a single output), or list of Numpy arrays (if the model has multiple outputs).
         :param batch_size: Integer or `None`. Number of samples per gradient update. If unspecified, `batch_size` will default to 256.
- 
+        :param epochs: Integer. Number of epochs to train the model. An epoch is an iteration over the entire `x` and `y` data provided. Note that in conjunction with `initial_epoch`, `epochs` is to be und
