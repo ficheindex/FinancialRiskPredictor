235e7575fa81b6cad9e0248a1c00e05ad80f6261
ficheindex <100076064+ficheindex@users.noreply.github.com>
2021-09-06 18:12:17 +0000

    added some code

--- model/deepfm_lib/models/basemodel.py
+++ model/deepfm_lib/models/basemodel.py
@@ -313,4 +313,6 @@ class BaseModel(nn.Module):
     def evaluate(self, x, y, batch_size=256):
         """
         :param x: Numpy array of test data (if the model has a single input), or list of Numpy arrays (if the model has multiple inputs).
-        :param y: Numpy array of target (label) data (if the model has a single output), or list of 
+        :param y: Numpy array of target (label) data (if the model has a single output), or list of Numpy arrays (if the model has multiple outputs).
+        :param batch_size: Integer or `None`. Number of samples per evaluation step. If unspecified, `batch_size` will default to 256.
+        :return: Dict co
