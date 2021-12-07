c7fe57afefe683ca9aa7e9267d09734219f0e210
ficheindex <100076064+ficheindex@users.noreply.github.com>
2021-12-07 22:43:52 +0000

    clean up specs

--- model/finpt_gpt.py
+++ model/finpt_gpt.py
@@ -4,4 +4,13 @@
 __author__ = "@YuweiYin"
 """
 
-from typing imp
+from typing import Optional, Tuple, Union
+import numpy as np
+
+import torch
+import torch.utils.checkpoint
+from torch import nn
+import torch.nn.functional as F
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+
+from transforme
