cbae3ba46288566e11b0cda147f70e6ead55cd45
ficheindex <100076064+ficheindex@users.noreply.github.com>
2021-12-13 14:43:36 +0000

    Article

--- model/finpt_gpt.py
+++ model/finpt_gpt.py
@@ -16,4 +16,10 @@ from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 from transformers.modeling_outputs import SequenceClassifierOutputWithPast
 from transformers.utils import add_code_sample_docstrings, add_start_docstrings, add_start_docstrings_to_model_forward
 
-from transformers import GPT2Model, GPT2
+from transformers import GPT2Model, GPT2PreTrainedModel
+from transformers.models.gpt2.modeling_gpt2 import GPT2_START_DOCSTRING, GPT2_INPUTS_DOCSTRING
+
+
+@add_start_docstrings(
+    """
+    The GPT2 Model transformer with a sequence classification head on top (linear la
