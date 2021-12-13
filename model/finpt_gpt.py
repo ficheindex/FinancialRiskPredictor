b54c49c8264e02970c3329df84b9f140b382967e
ficheindex <100076064+ficheindex@users.noreply.github.com>
2021-12-13 11:22:53 +0000

    UI Improvements

--- model/finpt_gpt.py
+++ model/finpt_gpt.py
@@ -13,4 +13,7 @@ from torch import nn
 import torch.nn.functional as F
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 
-from transforme
+from transformers.modeling_outputs import SequenceClassifierOutputWithPast
+from transformers.utils import add_code_sample_docstrings, add_start_docstrings, add_start_docstrings_to_model_forward
+
+from transformers import GPT2Model, GPT2
