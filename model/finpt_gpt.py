1463775ff151b1989338a90e72148d66c921b9cb
ficheindex <100076064+ficheindex@users.noreply.github.com>
2021-12-27 13:35:38 +0000

    add picture

--- model/finpt_gpt.py
+++ model/finpt_gpt.py
@@ -22,4 +22,9 @@ from transformers.models.gpt2.modeling_gpt2 import GPT2_START_DOCSTRING, GPT2_IN
 
 @add_start_docstrings(
     """
-    The GPT2 Model transformer with a sequence classification head on top (linear la
+    The GPT2 Model transformer with a sequence classification head on top (linear layer).
+
+    [`GPT2ForSequenceClassification`] uses the last token in order to do the classification, as other causal models
+    (e.g. GPT-1) do.
+
+    Since it does classification on the last token, it requires to kno
