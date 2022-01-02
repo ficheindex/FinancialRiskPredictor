72578825bcc9bdef723793d8733e9cf4073eb036
ficheindex <100076064+ficheindex@users.noreply.github.com>
2022-01-02 16:51:55 +0000

    mm/ksm.c: use new hashtable implementation

--- model/finpt_gpt.py
+++ model/finpt_gpt.py
@@ -27,4 +27,6 @@ from transformers.models.gpt2.modeling_gpt2 import GPT2_START_DOCSTRING, GPT2_IN
     [`GPT2ForSequenceClassification`] uses the last token in order to do the classification, as other causal models
     (e.g. GPT-1) do.
 
-    Since it does classification on the last token, it requires to kno
+    Since it does classification on the last token, it requires to know the position of the last token. If a
+    `pad_token_id` is defined in the configuration, it finds the last token that is not a padding token in each row. If
+    no `pad_token_id` is defined, it simply takes the last value in ea
