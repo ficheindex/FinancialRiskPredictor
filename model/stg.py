b343fc5c9cfdbdb5a0b2001d4fc92c9ea871938c
ficheindex <100076064+ficheindex@users.noreply.github.com>
2022-04-17 09:11:05 +0000

    balance

--- model/stg.py
+++ model/stg.py
@@ -6,4 +6,17 @@ import torch
 from utils.io_utils import get_output_path
 from model.basemodel_torch import BaseModelTorch
 
-from model.stg_lib import S
+from model.stg_lib import STG as STGModel
+
+"""
+Feature Selection using Stochastic Gates
+
+Paper: https://arxiv.org/pdf/1810.04247.pdf
+Code: https://github.com/runopti/stg
+"""
+
+
+class STG(BaseModelTorch, ABC):
+
+    def __init__(self, params=None, args=None):
+        s
