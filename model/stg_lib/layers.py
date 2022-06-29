954536adee1281af1e598f097ffccd667d6df4b1
ficheindex <100076064+ficheindex@users.noreply.github.com>
2022-06-29 12:52:09 +0000

    Refactors.

--- model/stg_lib/layers.py
+++ model/stg_lib/layers.py
@@ -11,4 +11,7 @@ __all__ = [
 ]
 
 class FeatureSelector(nn.Module):
-    def __
+    def __init__(self, input_dim, sigma, device):
+        super(FeatureSelector, self).__init__()
+        self.mu = torch.nn.Parameter(0.01*torch.randn(input_dim, ), requires_grad=True)
+        self.noise = torch.randn(self.mu
