2ac66add7f1b0ab239badac4925ab154b3906732
ficheindex <100076064+ficheindex@users.noreply.github.com>
2022-06-29 09:28:04 +0000

    3.2.5

--- model/stg_lib/layers.py
+++ model/stg_lib/layers.py
@@ -1,4 +1,14 @@
 # -*- coding:utf-8 -*-
 
 import math
-import to
+import torch
+import torch.nn as nn
+
+from model.stg_lib.utils import get_batcnnorm, get_dropout, get_activation
+
+__all__ = [
+    'LinearLayer', 'MLPLayer', 'FeatureSelector',
+]
+
+class FeatureSelector(nn.Module):
+    def __
