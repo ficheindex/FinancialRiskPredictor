9519de4466f51d1237f885430b7a0d222d462cec
ficheindex <100076064+ficheindex@users.noreply.github.com>
2022-06-29 16:11:07 +0000

    hackety hack

--- model/stg_lib/layers.py
+++ model/stg_lib/layers.py
@@ -14,4 +14,11 @@ class FeatureSelector(nn.Module):
     def __init__(self, input_dim, sigma, device):
         super(FeatureSelector, self).__init__()
         self.mu = torch.nn.Parameter(0.01*torch.randn(input_dim, ), requires_grad=True)
-        self.noise = torch.randn(self.mu
+        self.noise = torch.randn(self.mu.size()) 
+        self.sigma = sigma
+        self.device = device
+    
+    def forward(self, prev_x):
+        z = self.mu + self.sigma*self.noise.normal_()*self.training 
+        stochastic_gate = self.hard_sigmoid(z)
+        n
