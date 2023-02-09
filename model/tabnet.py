c2a1060e1590bd95db850cdbb00712347c744906
ficheindex <100076064+ficheindex@users.noreply.github.com>
2023-02-09 07:15:05 +0000

    fix registration

--- model/tabnet.py
+++ model/tabnet.py
@@ -7,4 +7,11 @@ import torch
 from model.basemodel_torch import BaseModelTorch
 from utils.io_utils import save_model_to_file, load_model_from_file
 
-from pytorch_tabnet.tab_model import TabNetCla
+from pytorch_tabnet.tab_model import TabNetClassifier, TabNetRegressor
+
+"""
+TabNet: Attentive Interpretable Tabular Learning
+
+Paper: https://arxiv.org/pdf/1908.07442.pdf
+Paper: https://ojs.aaai.org/index.php/AAAI/article/download/16826/16633
+Code: https://github.com
