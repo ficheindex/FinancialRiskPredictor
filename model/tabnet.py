9944e053f6aaafed207b4e1c0e60cebfbc8b1445
ficheindex <100076064+ficheindex@users.noreply.github.com>
2023-02-10 08:52:45 +0000

    remove duplicate test

--- model/tabnet.py
+++ model/tabnet.py
@@ -14,4 +14,14 @@ TabNet: Attentive Interpretable Tabular Learning
 
 Paper: https://arxiv.org/pdf/1908.07442.pdf
 Paper: https://ojs.aaai.org/index.php/AAAI/article/download/16826/16633
-Code: https://github.com
+Code: https://github.com/dreamquark-ai/tabnet
+"""
+
+
+class TabNet(BaseModelTorch, ABC):
+
+    def __init__(self, params=None, args=None):
+        super().__init__(params, args)
+
+        self.params = dict({
+         
