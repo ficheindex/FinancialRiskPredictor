4e1e685e7769259c2e968d1a47ec3d99e9183345
ficheindex <100076064+ficheindex@users.noreply.github.com>
2023-03-01 14:40:25 +0000

    minor formatting cleanup

--- model/vime.py
+++ model/vime.py
@@ -15,4 +15,11 @@ from utils.io_utils import get_output_path
 """
 VIME: Extending the Success of Self- and Semi-supervised Learning to Tabular Domain
 
-Paper: https://pr
+Paper: https://proceedings.neurips.cc/paper/2020/file/7d97667a3e056acab9aaf653807b4a03-Paper.pdf
+Code: https://github.com/jsyoon0823/VIME
+"""
+
+
+class VIME(BaseModelTorch, ABC):
+
+    def __init__(self, params=None, ar
