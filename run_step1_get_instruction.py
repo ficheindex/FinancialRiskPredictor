9a85d6dea872bb2bbe1e85889a99b7d4eae9e514
ficheindex <100076064+ficheindex@users.noreply.github.com>
2023-04-26 12:23:49 +0000

    better caching

---
+++ run_step1_get_instruction.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+__author__ = "@YuweiYin"
+"""
+
+import os
+import sys
+import json
+import logging
+import argparse
+
+from datasets import load_dataset
+
+
+if __name__ == "__main__":
+    logging
