707caab9496b17f3ac0660043b9bc9db6b9676ab
ficheindex <100076064+ficheindex@users.noreply.github.com>
2023-04-26 13:46:36 +0000

    remove failing test

--- run_step1_get_instruction.py
+++ run_step1_get_instruction.py
@@ -14,4 +14,10 @@ from datasets import load_dataset
 
 
 if __name__ == "__main__":
-    logging
+    logging.basicConfig(
+        format="[%(asctime)s - %(levelname)s - %(name)s] -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S", level=logging.INFO
+    )
+    logger = logging.getLogger(__name__)
+
+    parser = argparse.Arg
