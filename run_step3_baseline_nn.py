f7c65e4d15a8de34b8a46b958fa97c7324a4e432
ficheindex <100076064+ficheindex@users.noreply.github.com>
2023-07-25 08:26:14 +0000

    Script updates.

--- run_step3_baseline_nn.py
+++ run_step3_baseline_nn.py
@@ -22,4 +22,9 @@ from utils.seed import set_seed
 
 def run_baselines_nn(cur_ds_name, cur_model_name):
     logger.info(f"\n\n\n *** *** cur_ds_name: {cur_ds_name}; cur_model_name: {cur_model_name}")
-    data = load_dataset("yuweiyin/Fin
+    data = load_dataset("yuweiyin/FinBench", cur_ds_name, cache_dir=cache_ds)
+    train_set = data["train"] if "train" in data else []
+    val_set = data["validation"] if "train" in data else []
+    test_set = data["test"] if "train" in data else []
+
+    args.dev
