8cd2ad417d766b1f9735a440a801e1a318323875
ficheindex <100076064+ficheindex@users.noreply.github.com>
2023-09-11 20:00:21 +0000

    ss

--- run_step3_baseline_nn.py
+++ run_step3_baseline_nn.py
@@ -90,4 +90,6 @@ if __name__ == "__main__":
     parser.add_argument("--optimizer_name", type=str, default="AdamW", help="Specify which optimizer to use")
     parser.add_argument("--criterion_name", type=str, default="CrossEntropyLoss", help="Specify which criterion to use")
     parser.add_argument("--lr", type=float, default=float(3e-5), help="Learning rate")
-    parser.
+    parser.add_argument("--bsz", type=int, default=128, help="TrainingArguments: per_device_train/eval_batch_size")
+    parser.add_argument("--epoch", type=int, default=100, help="TrainingArguments: num_train_epochs")
+    parser.add_argum
