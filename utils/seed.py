ee86fa692e05631f6c9afdb82b46990d6ad9d594
ficheindex <100076064+ficheindex@users.noreply.github.com>
2023-09-25 15:37:32 +0000

    tracing: Fix irqs-off tag display in syscall tracing

--- utils/seed.py
+++ utils/seed.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
-# -*- coding: u
+# -*- coding: utf-8 -*-
+"""
+__author__ = "@YuweiYin"
+"""
+
+import random
+import numpy as np
+
+import torch
+from torch.backends import cudnn
+
+
+def set_seed(seed: int):
+    r"""
+    Sets the seed for generating random numbers in Py
