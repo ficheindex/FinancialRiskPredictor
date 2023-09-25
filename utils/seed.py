f93f9fb7a1f99808e4a2d629e7a1c18adc294e6a
ficheindex <100076064+ficheindex@users.noreply.github.com>
2023-09-25 20:21:42 +0000

    Delete .gitattributes

--- utils/seed.py
+++ utils/seed.py
@@ -13,4 +13,14 @@ from torch.backends import cudnn
 
 def set_seed(seed: int):
     r"""
-    Sets the seed for generating random numbers in Py
+    Sets the seed for generating random numbers in PyTorch, NumPy and Python.
+
+    Args:
+        seed (int): The desired seed.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    cudnn
